36a1f85e8a46da76630f7f006e0e4a7e4b7b68e0
EmilianoAdelarBade <58085172+emigrox@users.noreply.github.com>
2022-04-16 21:21:36 -0300

    La autenticacion funciona correcto con el user: admin y password: secret

--- src/main/java/com/productos/demo/Autenticacion.java
+++ src/main/java/com/productos/demo/Autenticacion.java
@@ -5,20 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
-
-    @Configuration
+@Configuration
     public class Autenticacion extends WebSecurityConfigurerAdapter
     {
         @Override
-        protected void configure(HttpSecurity http) throws Exception
-        {
-            http
-                    .csrf().disable()
-                    .authorizeRequests().anyRequest().authenticated()
-                    .and()
-                    .httpBasic();
+        protected void configure(AuthenticationManagerBuilder auth)
+                throws Exception {
+            PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+            auth.inMemoryAuthentication()
+                    .withUser("admin")
+                    .password(encoder.encode("secret"))
+                    .roles("USER");
         }
 
         @Autowired
@@ -27,7 +28,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
         {
             auth.inMemoryAuthentication()
                     .withUser("admin")
-                    .password("{noop}password")
+                    .password("secret")
                     .roles("USER");
         }
     }
